8621537c4663dd4e590cd81b7d5ab16bb9a3393f
joel-menezes <joel.menezes25@ycdsbk12.ca>
2025-05-15 23:18:05 -0400

    Finished sumofnaturalnumbers and added java docs

--- src/recursion/SumofNaturalNumbers.java
+++ src/recursion/SumofNaturalNumbers.java
@@ -1,6 +1,13 @@
 package recursion;
 
 public class SumofNaturalNumbers {
+    /**
+     * Description: Summing every single digit from n down to 1 sequentially
+     * 
+     * @param n number to sum
+     * @return int summed of all digits from n to 1
+     * @author Joel
+     */
     public static int sum(int n) {
         if (n == 1)
             return 1;
