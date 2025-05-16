cd90d84885ddebef8d7b8f48159fcd06517733b4
joel-menezes <joel.menezes25@ycdsbk12.ca>
2025-05-15 23:16:19 -0400

    Finished Sum of Digits added java docs

--- src/recursion/SumOfDigits.java
+++ src/recursion/SumOfDigits.java
@@ -1,6 +1,13 @@
 package recursion;
 
 public class SumOfDigits {
+    /**
+     * Description: Adding all the digits together result is returned
+     * 
+     * @param n the number you want the digits summed of
+     * @return int Sum of all the digits
+     * @author Joel
+     */
     public static int sumOfDigits(int n) {
         if (n == 0)
             return 0;
